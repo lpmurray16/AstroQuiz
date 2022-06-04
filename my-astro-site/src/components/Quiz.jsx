f2e1cc2bb217a23f0f81e5dc882c38b1b2ea5bcc
Logan Murray <lpmurray16@users.noreply.github.com>
2022-06-03 20:49:41 -0500

    first commit along with first working draft

---
+++ my-astro-site/src/components/Quiz.jsx
@@ -0,0 +1,184 @@
+import { useState, useEffect } from "react";
+import questionList from "../config/questions.js";
+import "../styles/quiz.css";
+
+const Quiz = () => {
+    const [question, setQuestion] = useState(questionList[0]);
+    const [answer, setAnswer] = useState("");
+    const [score, setScore] = useState(0);
+    const [questionNumber, setQuestionNumber] = useState(0);
+    const [isCorrect, setIsCorrect] = useState(false);
+    const [isAnswered, setIsAnswered] = useState(false);
+    const [isLastQuestion, setIsLastQuestion] = useState(false);
+    const [isOver, setIsOver] = useState(false);
+
+    useEffect(() => {
+        if (questionNumber === questionList.length - 1) {
+            setIsLastQuestion(true);
+        }
+    }, [questionNumber]);
+
+    const handleAnswer = (e) => {
+        setAnswer(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (answer === question.correctAnswer) {
+            setScore(score + 1);
+            setIsCorrect(true);
+        } else {
+            setIsCorrect(false);
+        }
+        setIsAnswered(true);
+    };
+
+    const handleNextQuestion = () => {
+        setQuestion(questionList[questionNumber + 1]);
+        setAnswer("");
+        setIsAnswered(false);
+        setQuestionNumber(questionNumber + 1);
+
+        if (questionNumber === questionList.length - 1) {
+            setIsOver(true);
+        }
+    };
+
+    const handleRestart = () => {
+        setQuestion(questionList[0]);
+        setAnswer("");
+        setScore(0);
+        setQuestionNumber(0);
+        setIsCorrect(false);
+        setIsAnswered(false);
+        setIsOver(false);
+    };
+
+    return (
+        <div className="quiz-container">
+            <div className="quiz-question">
+                {isOver ? (
+                    <div className="quiz-question__over">
+                        <h2 className="quiz-question__text">
+                            That's All Folks! You scored {score} out of{" "}
+                            {questionList.length} questions correctly.
+                        </h2>
+                        <p>
+                            {(score / questionList.length) * 100}% of the
+                            questions were answered correctly. <br></br>
+                            <br></br>
+                            {(() => {
+                                if (score / questionList.length > 0.75) {
+                                    return " You're a Harry Potter nerd!";
+                                } else if (score / questionList.length > 0.5) {
+                                    return " You can do better. Just saying.";
+                                } else if (score / questionList.length > 0.25) {
+                                    return " You're terrible, kys.";
+                                } else {
+                                    return " You need to jump in a lava lake. Or read more HP books.";
+                                }
+                            })()}
+                        </p>
+                        <button className="button" onClick={handleRestart}>
+                            Restart
+                        </button>
+                    </div>
+                ) : (
+                    <div className="quiz-question__container">
+                        <h2 className="quiz-question__text">
+                            {questionNumber + 1}) {question.question}
+                        </h2>
+                        <form onSubmit={handleSubmit}>
+                            <div className="quiz-question__choices">
+                                {question.choices.map((c, index) => (
+                                    <label className="label" key={index}>
+                                        <input
+                                            type="radio"
+                                            name="choice"
+                                            value={c}
+                                            onChange={handleAnswer}
+                                            checked={
+                                                answer === c ? true : false
+                                            }
+                                        />
+                                        <span className="checkmark"></span>
+                                        {c}
+                                    </label>
+                                ))}
+                            </div>
+                            <button
+                                type="submit"
+                                style={{
+                                    display: !isAnswered ? "block" : "none",
+                                }}
+                                className="button"
+                            >
+                                Submit
+                            </button>
+                        </form>
+                        <div className="quiz-question__next">
+                            <button
+                                className="button"
+                                style={{
+                                    display: isAnswered ? "block" : "none",
+                                }}
+                                onClick={handleNextQuestion}
+                            >
+                                Next Question &rarr;
+                            </button>
+                        </div>
+                        {isAnswered && (
+                            <div className="quiz-question__feedback">
+                                {isCorrect ? (
+                                    <div className="quiz-question__feedback--correct">
+                                        <p>Correct!✅</p>
+                                    </div>
+                                ) : (
+                                    <div className="quiz-question__feedback--incorrect">
+                                        <p>Incorrect!❌</p>
+                                        <p>
+                                            The correct answer was:{" "}
+                                            <span
+                                                style={{
+                                                    fontWeight: "bold",
+                                                    fontStyle: "italic",
+                                                }}
+                                            >
+                                                {question.correctAnswer}
+                                            </span>
+                                        </p>
+                                    </div>
+                                )}
+                            </div>
+                        )}
+                        <div className="quiz-question__feedback">
+                            <p>
+                                You've scored {score} out of{" "}
+                                {questionList.length}
+                            </p>
+                            <div className="progress_bar--container">
+                                <label htmlFor="completion_bar">
+                                    Progress:{" "}
+                                </label>
+                                <progress
+                                    id="completion_bar"
+                                    value={questionNumber}
+                                    max={questionList.length}
+                                ></progress>
+                            </div>
+
+                            <button
+                                className="button button--restart"
+                                onClick={handleRestart}
+                            >
+                                Restart Quiz
+                            </button>
+                        </div>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default Quiz;
---
+++ my-astro-site/src/config/questions.js
@@ -0,0 +1,93 @@
+const questionList = [
+    {
+        id: 1,
+        question: "What is Harry Potter's middle name?",
+        choices: ["Kirk", "Severus", "James", "Bilius"],
+        correctAnswer: "James",
+    },
+    {
+        id: 2,
+        question: "What is the name of Harry Potter's pet owl?",
+        choices: ["Hedwig", "Ron", "Twig", "NightWing"],
+        correctAnswer: "Hedwig",
+    },
+    {
+        id: 3,
+        question: "What is the name of Harry Potter's best friend?",
+        choices: ["Ron", "Hermione", "Ginny", "Draco"],
+        correctAnswer: "Ron",
+    },
+    {
+        id: 4,
+        question: "What subject does Professor McGonagall teach?",
+        choices: ["Flying", "Transfiguration", "Potions", "Charms"],
+        correctAnswer: "Transfiguration",
+    },
+    {
+        id: 5,
+        question: "Where did Harry and his parents live (and die)?",
+        choices: ["Frankfurt", "London", "Godrics Hollow", "Hogsmeade"],
+        correctAnswer: "Godrics Hollow",
+    },
+    {
+        id: 6,
+        question: "What is the name of Ron Weasley's pet rat?",
+        choices: ["Tom", "Molly", "Scabbers", "Mr. Whiskers"],
+        correctAnswer: "Scabbers",
+    },
+    {
+        id: 7,
+        question:
+            "What is the name of the large, red, leather ball thrown and passed during a Quidditch match?",
+        choices: ["Goober", "Snitch", "Bludger", "Quaffle"],
+        correctAnswer: "Quaffle",
+    },
+    {
+        id: 8,
+        question: "What Hogwarts House do Harry and his friends belong to?",
+        choices: ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"],
+        correctAnswer: "Gryffindor",
+    },
+    {
+        id: 9,
+        question:
+            "What is the name of the tree that Ron and Harry crash into in book/movie #2?",
+        choices: [
+            "Thestral Tree",
+            "Whomping Willow",
+            "Crab Tree",
+            "Hollyhock Maple",
+        ],
+        correctAnswer: "Whomping Willow",
+    },
+    {
+        id: 10,
+        question: "What is the model of Harry Potter's first broom?",
+        choices: ["Nimbus 2000", "Nimbus 3000", "Nimbus 4000", "Nimbus 9000"],
+        correctAnswer: "Nimbus 2000",
+    },
+    {
+        id: 11,
+        question: "What is the name of Harry Potter's Godfather?",
+        choices: [
+            "Severus Snape",
+            "Albus Dumbledore",
+            "Peter Petigrew",
+            "Sirius Black",
+        ],
+        correctAnswer: "Sirius Black",
+    },
+    {
+        id: 12,
+        question: "What platform is the Hogwarts Express located?",
+        choices: [
+            "Platform 9 and 1/2",
+            "Platform 9 and 3/4",
+            "Platform 10",
+            "Platform 8 and 2/3",
+        ],
+        correctAnswer: "Platform 9 and 3/4",
+    },
+];
+
+export default questionList;
